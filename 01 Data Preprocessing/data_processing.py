70466c4c7e7a57f5766bcb659e2cab82cc445fd4
Ashish Rawat <ashish.rawat@signify.com>
2018-09-01 18:26:14 +0530

    data processing file added

---
+++ 01 Data Preprocessing/data_processing.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv('Data.csv')
+
+independent = dataset.iloc[:,:-1].values
+#integer location based from 0 to length-1
+# takes all values except last column from [:,:-1]
+
+
+print(independent)
+print('\n\n')
+
+
+
+dependent = dataset.iloc[:,3].values
+#integer location based from 0 to length-1
+print(dependent)
